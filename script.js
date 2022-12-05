d741221f44df6e72c00d3263e5e4d380256dee35
Fox <taylorcasse@gmail.com>
2022-12-05 17:46:33 +0200

    Basic mechanics working, need to impliment winCheck

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const squareSideLength = 50;
+
 const PlayerFactory = (name, mark) => {
     let gamesWon = 0;
     let gamesLost = 0
@@ -10,7 +12,7 @@ const PlayerFactory = (name, mark) => {
     }
 
     return {
-        name,
+        mark,
         introduceSelf
     }
 }
@@ -20,46 +22,88 @@ const Board = (function(dim){
     const element = document.createElement('div');
     element.classList.add('board');
 
+    element.style.width = dim * squareSideLength + 'px';
+    element.style.height = dim * squareSideLength + 'px';
+
+    const matrix = [];
+
     const reset = () => {
+        const Square = (x, y) => {
+            const squareDiv = document.createElement('div');
+            squareDiv.classList.add('square');
 
-    } 
-    const SquareFactory = () => {
-        const element = document.createElement('div');
-        element.classList.add('square');
+            let populated = false;
 
-        let currentMark = null;
-
-        const populate = (mark) => {
-            if (!currentMark) {
-                currentMark = mark;
-                element.textContent = mark;
+            const populate = function () {
+                populated = true;
             }
+    
+    
+            squareDiv.addEventListener('click', () => {
+                if (!populated) {
+                    squareDiv.textContent = game.getMark()
+                    game.takeTurn(x, y);
 
-            
+                } 
+            })
+
+            return {squareDiv, populate, populated}
+        }
+    
+        let square;
+        for (let i = 0; i < dim; i++) {
+            cols = [];
+            for (let j = 0; j < dim; j++) {
+                square = Square(j, i);
+                element.appendChild(square.squareDiv);
+                cols.push(square)
+            }
+    
+            matrix.push(cols);
         }
 
-        return {element, populate}
+        document.querySelector('.main').appendChild(element);
+    } 
 
+    const getMatrix = function () {
+        return matrix;
     }
-
-    const array = [];
-    for (let i = 0; i < dim; i++) {
-        cols = [];
-        for (let j = 0; j < dim; j++) {
-            cols.push(SquareFactory());
-        }
-
-        array.push(cols);
-
-    }
-
-    console.log(array)
+    
+    return {reset, getMatrix};
 
 }(3))
 
-const p1 = PlayerFactory("James", 'X');
-p1.introduceSelf();
+const game = (function(){
 
-// const GameModule = (function(){
+    const player1 = PlayerFactory("James", 'X');
+    const player2 = PlayerFactory('Chuck', 'O');
 
-// }())
+    let currentPlayer = player1;
+    let numTurns = 0;
+    let currentMark = currentPlayer.mark;
+    Board.reset();
+
+    function winCheck (matrix) {
+
+    }
+
+
+    function takeTurn (x, y) {
+        let matrix = Board.getMatrix()
+        let activeSquare = matrix[x][y];
+
+        activeSquare.populated = true;
+        activeSquare.textContent = currentMark;
+
+        numTurns += 1;
+        currentPlayer = numTurns % 2 ? player2 : player1;
+        currentMark = currentPlayer.mark;
+    }
+
+    function getMark () {
+        return currentMark;
+    }
+
+    return {takeTurn, getMark}
+
+}())
